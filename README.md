a98cdb5320f8ec6cd72d416505e5eb6599c89ab8
paraskumar <parasbarnwal06@gmail.com>
2019-07-22 14:21:59 +0530

    Added i

---
+++ README.md
@@ -0,0 +1,3 @@
+# C Plus Plus Preprocessor
+
+This is a cpp preprocessor written in python.
---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='CPP Preprocessor',
+    version='0.1.0',
+    packages=['preprocessor', ],
+    license='GNU AFFERO GENERAL PUBLIC LICENSE',
+    long_description=open('README.md').read(),
+)
